f2fa85cec0124e828f93a4922d02adc6c44d9d04
Aman Kumar <2019200@iiitdmj.ac.in>
2021-10-10 21:48:19 +0530

    error resolving

--- pages/Page2.js
+++ pages/Page2.js
@@ -306,9 +306,9 @@ const Review = () => {
 							width='4456.51'
 							height='2262'
 							filterUnits='userSpaceOnUse'
-							color-interpolation-filters='sRGB'
+							colorInterpolationFilters='sRGB'
 						>
-							<feFlood flood-opacity='0' result='BackgroundImageFix' />
+							<feFlood floodOpacity='0' result='BackgroundImageFix' />
 							<feColorMatrix
 								in='SourceAlpha'
 								type='matrix'
